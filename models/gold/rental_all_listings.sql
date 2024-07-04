4d420ad5240e32358ef2585c0dc9a676d03a7957
Gean Pannellini <geanpanne@gmail.com>
2024-07-04 01:40:47 +0000

    feat: create gold and analyses layers

--- models/gold/rental_all_listings.sql
+++ models/gold/rental_all_listings.sql
@@ -6,10 +6,15 @@
 
 SELECT
     fp.listing_id,
-    dp.region,
-    dp.price
+    dp.housing_type,
+    l.locality,
+    dp.price,
+    dp.sqfeet
 FROM
     {{ ref('fact_rental_listings') }} AS fp
 LEFT JOIN
    {{ ref('dim_property_characteristics')}} AS dp
    ON fp.property_char_key = dp.property_char_key
+LEFT JOIN
+   {{ ref('dim_locality')}} AS l
+   ON fp.locality_key = fp.locality_key
